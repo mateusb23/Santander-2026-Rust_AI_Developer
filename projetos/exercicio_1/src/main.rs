8ed3c1cc7f7fb410fc2a58c8bcfc59260e25978c
mateusb23 <mateusb.bispo@gmail.com>
2026-06-10 18:38:27 -0300

    Atualizando conhecimentos sobre loop e match

--- projetos/exercicio_1/src/main.rs
+++ projetos/exercicio_1/src/main.rs
@@ -1,21 +1,48 @@
 fn main() {
-    println!("Digite o valor da tabuada que deseja calcular:");
+    loop {
+        println!("Digite o valor da tabuada que deseja calcular:");
+        println!(
+            r#"
+            Opção 1
+            Opção 2
+            Opção 3
+            Opção 4 - Sair
+            "#
+        );
 
-    let mut input = String::new();
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("Falha ao ler a entrada");
+        let mut opcao: String = String::new();
+        std::io::stdin()
+            .read_line(&mut opcao)
+            .expect("Falha ao ler a entrada");
 
-    let numero: u8 = input
-        .trim()
-        .parse()
-        .expect("Falha ao converter para número");
+        let opcao: u8 = opcao
+            .trim()
+            .parse()
+            .expect("Falha ao converter para número");
 
-    tabuada(numero);
-}
-
-fn tabuada(numero: u8) {
-    for i in 1..=10 {
-        println!("{} x {} = {}", numero, i, numero * i);
+        match opcao {
+            1 => {
+                println!("Você escolheu a opção Um:");
+            }
+            2 => {
+                println!("Você escolheu a opção Dois:");
+            }
+            3 => {
+                println!("Você escolheu a opção Três:");
+            }
+            4 => {
+                println!("Saindo do programa...");
+                break;
+            }
+            _ => {
+                println!("Opção inválida!");
+            }
+        }
     }
 }
+
+// fn tabuada(numero: u8) {
+//     for i in 1..=10 {
+//         println!("{} x {} = {}", numero, i, numero * i);
+//     }
+// }
